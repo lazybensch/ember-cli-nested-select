87644668f7e70277fdf497e2e276c5161ac8b582
Ben <benjamin.schoenburg@madvertise.com>
2015-03-31 15:14:09 +0200

    filter and wrap

--- addon/components/drop-down.js
+++ addon/components/drop-down.js
@@ -1,12 +1,33 @@
 import Ember from 'ember';
 import layout from '../templates/components/drop-down';
+import computedFilterByQuery from 'ember-cli-filter-by-query';
 
 export default Ember.Component.extend({
   layout: layout,
   classNames: 'drop-down',
 
+  idKey: 'id',
+  infoKey: 'info',
+  labelKey: 'label',
+  childrenKey: 'children',
+
+  convertedContent: Ember.computed.map('content', function(entry) {
+    return this.wrap(entry);
+  }),
+
+  filteredContent: computedFilterByQuery('convertedContent', 'label', 'query'),
+
+  wrap: function(entry) {
+    return {
+      id: Ember.get(entry, this.get('idKey')),
+      info: Ember.get(entry, this.get('infoKey')),
+      label: Ember.get(entry, this.get('labelKey')),
+      children: Ember.get(entry, this.get('childrenKey'))
+    };
+  },
+
   didInsertElement: function() {
-    var el = this.$()
+    var el = this.$();
     var list = el.find('.list');
     list.appendTo('body');
     list.css({
