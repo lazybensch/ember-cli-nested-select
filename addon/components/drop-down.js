ac50d1a823e686106a8a901927bb140f9a3b15e4
Ben <benjamin.schoenburg@madvertise.com>
2015-03-31 14:50:22 +0200

    detach dropdown from DOM

--- addon/components/drop-down.js
+++ addon/components/drop-down.js
@@ -3,5 +3,16 @@ import layout from '../templates/components/drop-down';
 
 export default Ember.Component.extend({
   layout: layout,
-  classNames: 'drop-down'
+  classNames: 'drop-down',
+
+  didInsertElement: function() {
+    var el = this.$()
+    var list = el.find('.list');
+    list.appendTo('body');
+    list.css({
+      'position': 'absolute',
+      'top': el.offset().top + el.height(),
+      'left': el.offset().left,
+    });
+  }
 });
